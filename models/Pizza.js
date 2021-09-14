d690571c2b51ac65b7a66d1dac1b7f372d94a520
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-09-14 13:56:18 -0400

    user routes

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// create the Pizza model using the pizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the pizza model
+module.exports = Pizza;
